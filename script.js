d8f370fd47ade4a7c77b023d3fa203c585b58650
sandrorios <sandroderios@hotmail.com>
2023-10-28 15:06:42 -0400

    code to work on

--- script.js
+++ script.js
@@ -11,4 +11,40 @@ for(element of elements){
             this.removeAttribute('style');
         }
     })
+    addIcon();
 }
+
+
+function addIcon(){
+    let item = document.querySelectorAll('.text');
+   let button = createButton('remove-item text-red');
+  item.forEach((item) =>{
+    item.appendChild(button);
+  })
+
+   
+//   for(i = 0; i < item.length; i++){
+//    icon += item[i];
+//   }
+   
+  
+  // console.log(item)
+  
+  console.log(item)
+}
+
+function createButton(classes){
+    let button = document.createElement('button');
+    button.className = classes;
+    let icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
+    return button;
+}
+
+function createIcon(classes){
+    let icon = document.createElement('i');
+    icon.className = classes;
+    console.log(icon)
+    return icon;
+}
+
